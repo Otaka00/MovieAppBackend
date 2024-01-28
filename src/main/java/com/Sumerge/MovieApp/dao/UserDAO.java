46d2e6039bed9d8303fc65777fc330cee46f64ce
otaka00 <ahmad_otaca@hotmail.com>
2024-01-28 16:41:44 +0200

    Add User DAO

---
+++ src/main/java/com/Sumerge/MovieApp/dao/UserDAO.java
@@ -0,0 +1,7 @@
+package com.Sumerge.MovieApp.dao;
+
+import com.Sumerge.MovieApp.entity.User;
+
+public interface UserDAO {
+    void save(User user);
+}
---
+++ src/main/java/com/Sumerge/MovieApp/dao/UserDAOImpl.java
@@ -0,0 +1,24 @@
+package com.Sumerge.MovieApp.dao;
+
+import com.Sumerge.MovieApp.entity.User;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+}
