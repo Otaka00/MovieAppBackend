ba3e0b03352feaf707085f1d16921229876c8f8d
otaka00 <ahmad_otaca@hotmail.com>
2024-01-30 22:51:44 +0200

    Edit User Entity

--- src/main/java/com/Sumerge/MovieApp/entity/User.java
+++ src/main/java/com/Sumerge/MovieApp/entity/User.java
@@ -28,44 +28,6 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String pass;
 
-    public User(String email, String pass) {
-        this.email = email;
-        this.pass = pass;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPassword(String password) {
-        this.pass = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + pass + '\'' +
-                '}';
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
