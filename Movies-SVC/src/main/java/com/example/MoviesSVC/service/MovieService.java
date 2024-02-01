2ab595a69297ced6d962f3ca1409bc447b2620fd
otaka00 <ahmad_otaca@hotmail.com>
2024-02-02 01:29:14 +0200

    Add Security Dependency

--- Movies-SVC/src/main/java/com/example/MoviesSVC/service/MovieService.java
+++ Movies-SVC/src/main/java/com/example/MoviesSVC/service/MovieService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 //import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
