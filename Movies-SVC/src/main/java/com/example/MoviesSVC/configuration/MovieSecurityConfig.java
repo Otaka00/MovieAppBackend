de67604259f23cb9b352cd3058db10209580b450
otaka00 <ahmad_otaca@hotmail.com>
2024-02-02 17:35:03 +0200

    Update MovieSecurityConfig

--- Movies-SVC/src/main/java/com/example/MoviesSVC/configuration/MovieSecurityConfig.java
+++ Movies-SVC/src/main/java/com/example/MoviesSVC/configuration/MovieSecurityConfig.java
@@ -1,4 +1,4 @@
-//package com.example.MoviesSVC.configuration;
+package com.example.MoviesSVC.configuration;//package com.example.MoviesSVC.configuration;
 //
 //import com.Sumerge.MovieApp.configuration.JwtAuthenticationFilter;
 //import com.Sumerge.MovieApp.configuration.SecurityConfig;
@@ -45,3 +45,39 @@
 //        return http.build();
 //    }
 //}
+
+
+import com.example.MoviesSVC.AuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class MovieSecurityConfig {
+
+    private final AuthFilter authFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
