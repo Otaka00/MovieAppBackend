fc06117615a84717f83e4b3090674dcdb128b29e
otaka00 <ahmad_otaca@hotmail.com>
2024-02-03 16:04:34 +0200

    Deleted Global Variables class

--- Movies-SVC/src/main/java/com/example/MoviesSVC/GlobalVariables.java
+++
@@ -1,8 +0,0 @@
-package com.example.MoviesSVC;
-
-public class GlobalVariables {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public final String AUTH_URL = "http://localhost:8080/api/v1/auth/validate";
-
-}
